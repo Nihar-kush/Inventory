583de65ec5c249dc3ecba0ca0b03044ff158fa74
nihar kushwaha <niharkushwaha7@gmail.com>
2022-07-01 21:46:54 +0530

    1st commit

---
+++ src/Pages/Home.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+export default function Home() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function registerUser() {
+    const response = await fetch("/register", {
+      method: "POST",
+      body: JSON.stringify({ name, email, password }),
+    });
+    const data = await response.json;
+    console.log(data);
+  }
+
+  return (
+    <div>
+      <form onSubmit={registerUser}>
+        <input
+          type="text"
+          value={name}
+          placeholder="Name"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+        <input
+          type="email"
+          value={email}
+          placeholder="Email"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <input
+          type="password"
+          value={password}
+          placeholder="Password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <input type="submit" value="Register" />
+      </form>
+    </div>
+  );
+}
